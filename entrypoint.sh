31d7559e303cc7c59ba3e99305183bda9ddf581b
Kieran Gleeson <kgleeson@gmail.com>
2021-08-01 12:57:04 +0100

    Fixing crontab env

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 #Run on launch
 python display.py
 
+#Export Docker env to container
+printenv > /etc/environment
+
 echo "*/5 * * * * cd /code ; /usr/local/bin/python display.py >> /var/log/cron.log 2>&1
 # "> schedule.txt
 
